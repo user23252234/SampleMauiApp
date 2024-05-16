91c7c03686a7b749bbc1cf84e3bb17d90045e619
user23252234 <Tizian.Michel@doppelmayr.com>
2024-05-16 16:46:38 +0200

    Add files via upload

---
+++ SampleMauiApp/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SampleMauiApp.ViewModels;
+
+namespace SampleMauiApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
